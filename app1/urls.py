f4bca9a9e912dd18bc53d3f708526032a398c068
sarunkumar1729 <sarunkumarvv1993@gmail.com>
2023-11-05 17:38:48 +0530

    third commit

--- app1/urls.py
+++ app1/urls.py
@@ -4,7 +4,7 @@ from .import views
 urlpatterns=[
     path('',views.index,name='index'),
     path('register',views.register,name='register'),
-    path('login',views.login,name='login'),
+    path('userlogin',views.userlogin,name='userlogin'),
     path('check_username/', views.check_username, name='check_username'),
-
+    path('userlogout',views.userlogout,name='userlogout')
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import JsonResponse
 from .models import User
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 
@@ -15,17 +16,30 @@ def register(request):
         email=request.POST['email']
         password=request.POST['password']
         confirmPassword=request.POST['password1']
-        print(username,firstname,lastname,email,password,confirmPassword)
-        return redirect('login')
+        # print(username,firstname,lastname,email,password,confirmPassword)
+        user=User.objects.create_user(username=username,first_name=firstname,last_name=lastname,email=email,password=password)
+        user.save()
+        print('success')
+        return redirect('userlogin')
     else:
         return render(request,'register.html')
     
-def login(request):
+def userlogin(request):
     if request.method=='POST':
-        pass
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+        else:
+            return render(request,'login.html')
     else:
         return render(request,'login.html')
     
+def userlogout(request):
+    logout(request)
+    return redirect('index')
 
 def check_username(request):
     if request.method == 'GET':
